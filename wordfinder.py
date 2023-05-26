847b979a62a4a9284370b19bc7e99366318ef395
Camryn Scott <camryn1242@yahoo.com>
2023-05-25 21:04:17 -0400

    imported Random

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,15 @@
 """Word Finder: finds random words from a dictionary."""
-
+import random
 
 class WordFinder:
-    ...
+    
+    def __repr__(self):
+        f"Words:{line}"
+        
+        
+
+    def get_words():
+        file = open("words.txt", "r")
+        for line in file:
+            print(line)
+            file.close()
