a0d9d7ab40c81158ded62fd2794e6a114d6f61c5
Camryn Scott <camryn1242@yahoo.com>
2023-05-25 21:08:45 -0400

    added final class SpecialWordFinder

--- wordfinder.py
+++ wordfinder.py
@@ -12,7 +12,7 @@ class WordFinder:
         print(f"{len(self.words)} words read")
 
     def parse(self, file):
-        """Parse dict_file -> list of words."""
+        """Parse file -> list of words."""
 
         return [w.strip() for w in file]
 
@@ -20,3 +20,26 @@ class WordFinder:
         """Return random word."""
 
         return random.choice(self.words)
+    
+
+class SpecialWordFinder(WordFinder):
+    """Specialized WordFinder that excludes blank lines/comments.
+    
+    >>> swf = SpecialWordFinder("complex.txt")
+    3 words read
+
+    >>> swf.random() in ["pear", "carrot", "kale"]
+    True
+
+    >>> swf.random() in ["pear", "carrot", "kale"]
+    True
+
+    >>> swf.random() in ["pear", "carrot", "kale"]
+    True
+    """
+
+    def parse(self, dict_file):
+        """Parse dict_file -> list of words, skipping blanks/comments."""
+
+        return [w.strip() for w in dict_file
+                if w.strip() and not w.startswith("#")]
